e4d084ee2af4918f378e7e8fd1d12f0578738583
zhihan.wang <zhihan.wang@baishancloud.com>
2021-02-22 23:22:52 +0800

    add first

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import utils.PDFUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        /*String fileName = "/Users/wangzhihan/Downloads/baiduyun/【咖啡教室】咖啡简史.pdf";  //这里先手动把绝对路径的文件夹给补上。
+        String outName = "【咖啡教室】咖啡简史.txt";
+        PDFUtil pdfUtil = new PDFUtil();
+        pdfUtil.readPDF(fileName,outName);*/
+        System.out.println("hello world");
+    }
+}
